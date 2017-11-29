7f72fca39eb4077af15cc0d7fad5733f7c90eebb
ashleydear <ashley@dear.com>
2017-11-29 17:46:25 -0500

    profile now has things

--- myProfile.php
+++ myProfile.php
@@ -6,7 +6,6 @@ $db_connection = new mysqli($host, $user, $dbpassword, $database);
 if ($db_connection->connect_error){
     die($db_connection->connect_error);
 }
-print_r($_POST);
 
 session_start();
 $email = $_SESSION['UserEmail'];
@@ -18,12 +17,75 @@ $result = $db_connection->query($query);
 
     $firstName = $row['firstName'];
     $lastName = $row['lastName'];
-    $telephonerNumber = $row['telephoneNumber'];
+    $telephoneNumber = $row['telephoneNumber'];
     $birthday= $row['birthday'];
     $food = $row['food'];
     $text = $row['text'];
+    $timeAvail = $row['specifications'];
 
-    print_r($row);
+    $timeAvail = explode(" ", $timeAvail);
+    $firstHour = explode(":", $timeAvail[0]);
+    $secondHour = explode(":", $timeAvail[1]);
+
+    if ($firstHour[0] < 13) {
+        $firstAmOrPm = "am";
+    } else {
+        $firstHour[0] = $firstHour[0] - 12;
+        $firstAmOrPm = "pm";
+    }
+
+    if ($secondHour[0] < 13) {
+        $secondAmOrPm = "am";
+    } else {
+        $secondHour[0] = $secondHour[0] - 12;
+        $secondAmOrPm = "pm";
+    }
+
+    $food = explode(",", $food);
+    $food = implode(", ", $food);
+
+    $birthday = explode("-", $birthday);
+
+    switch ($birthday[1]) {
+    case 1:
+        $birthday[1] = "January";
+        break;
+    case 2:
+        $birthday[1] = "February";
+        break;
+    case 3:
+        $birthday[1] = "March";
+        break;
+    case 4:
+        $birthday[1] = "April";
+        break;
+    case 5:
+        $birthday[1] = "May";
+        break;
+    case 6:
+        $birthday[1] = "June";
+        break;
+    case 7:
+        $birthday[1] = "July";
+        break;
+    case 8:
+        $birthday[1] = "August";
+        break;
+    case 9:
+        $birthday[1] = "September";
+        break;
+    case 10:
+        $birthday[1] = "October";
+        break;
+    case 11:
+        $birthday[1] = "November";
+        break;
+    case 12:
+        $birthday[1] = "December";
+        break;
+    }
+
+    $birthday[3] = 2017 - $birthday[0];
 
 
 $body = <<<BODY
@@ -35,18 +97,38 @@ $body = <<<BODY
         <link rel="stylesheet" type="text/css" href="myProfile.css"/>
     </head>
     <body>
-    <h2>My Profile</h2>
+    <h2>This is your profile, $firstName $lastName.</h2>
 
     <img src="defaultProfile.png" alt="Profile Picture" style="width: 25%; border-style: solid; border-color: black;">
     <br><br>
-    <strong>Favorite Kinds of Foods:</strong>
+    <strong>Name:</strong>
+        $firstName $lastName
     <br><br>
     <strong>Bio:</strong>
+        $text
     <br><br>
-    <strong>Contact Information: {$telephonerNumber}</strong>
+    <strong>Favorite Kinds of Foods:</strong>
+        $food
+    <br><br>
+    <strong>Time Availability:</strong>
+        $firstHour[0]:$firstHour[1]$firstAmOrPm - $secondHour[0]:$secondHour[1]$secondAmOrPm
+    <br><br>
+    <strong>Contact Information:</strong>
+        $telephoneNumber
+    <br><br>
+    <strong>Birthday:</strong>
+        $birthday[1] $birthday[2]
+    <br><br>
+    <strong>Age:</strong>
+        $birthday[3]
     <br><br>
 
-    <form action="homepage.html" method="POST">
+    <form action="editProfile.php" method="POST">
+        <input type="submit" name="edit" value="Edit My Profile"/>
+    </form>
+    <br>
+
+    <form action="menu.html" method="POST">
         <input type="submit" name="home" value="Go Home"/>
     </form>
 </body>
